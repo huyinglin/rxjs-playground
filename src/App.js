14c347ff3ee12de550f46a3f96ba8af8316f59ee
congbai.hyl <congbai.hyl@alibaba-inc.com>
2021-01-22 10:27:34 +0800

    feat: update

--- src/App.js
+++ src/App.js
@@ -1,191 +1,309 @@
 import React, { useEffect } from 'react';
-import { Subject } from 'rxjs';
-import { scan } from 'rxjs/operators';
-import { styled } from '@linaria/react';
-import './App.css';
-
-const CounterView = ({count, onIncrement, onDecrement}) => (
-  <div>
-    <h1>Count: {count}</h1>
-    <button onClick={onIncrement}>+</button>
-    <button onClick={onDecrement}>-</button>
-  </div>
-);
-
-const Container = styled.div`
-  color: red;
-`;
-
-class RxCounter extends React.Component {
-  constructor() {
-    super(...arguments);
-
-    this.state = {count: 0};
-
-    this.counter = new Subject();
-
-    this.counter
-      .pipe(scan((result, inc) => result + inc, 0))
-      .subscribe(value => this.setState({count: value}));
-  }
-
-  render() {
-    return <CounterView
-      count={this.state.count}
-      onIncrement={()=> this.counter.next(1)}
-      onDecrement={()=> this.counter.next(-1)}
-    />
-  }
-}
+import { Observable, Subject, interval, range, asapScheduler, fromEvent } from 'rxjs';
+import { scan, take, map } from 'rxjs/operators';
 
 function App() {
 
+/* ============================== 创建 Observable ============================= */
+  useEffect(() => {
+
+    const onSubscribe = observer => {
+      observer.next(1);
+      observer.next(2);
+      observer.next(3);
+    };
+    const source$ = new Observable(onSubscribe);
+
+    const theObserver = {
+      next: val => console.log(val),
+    };
+    source$.subscribe(theObserver);
+
+  }, []);
+
+// /* ============================ 跨越时间的 Observable ============================ */
+  useEffect(() => {
+
+    const onSubscribe = observer => {
+      observer.next(1);
+      observer.next(2);
+      observer.next(3);
+
+      setTimeout(() => {
+        observer.next(4);
+      }, 2 * 1000);
+    };
+    const source$ = new Observable(onSubscribe);
+
+    const theObserver = {
+      next: val => console.log(val),
+    };
+    source$.subscribe(theObserver);
+
+  }, []);
+
+// /* ============================ 永无止境的 Observable ============================ */
+//   useEffect(() => {
+
+//     const onSubscribe = observer => {
+//       observer.next(1);
+//       observer.next(2);
+//       observer.next(3);
+
+//       let number = 1;
+//       setInterval(() => {
+//         observer.next(number++);
+//       }, 1000);
+//     };
+//     const source$ = new Observable(onSubscribe);
+
+//     const theObserver = {
+//       next: val => console.log(val),
+//     };
+//     source$.subscribe(theObserver);
+
+//   }, []);
+
+// // /* ============================= Observable 的完结 ============================= */
+//   useEffect(() => {
+
+//     const onSubscribe = observer => {
+//       observer.next(1);
+//       observer.next(2);
+//       observer.next(3);
+
+//       let number = 1;
+
+//       const timer = setInterval(() => {
+//         observer.next(number++);
+
+//         if (number > 3) {
+//           clearInterval(timer);
+//         }
+//       }, 1000);
+//     };
+//     const source$ = new Observable(onSubscribe);
+
+//     const theObserver = {
+//       next: val => console.log(val),
+//     };
+//     source$.subscribe(theObserver);
+
+//   }, []);
+
+//   useEffect(() => {
+
+//     const onSubscribe = observer => {
+//       observer.next(1);
+//       observer.next(2);
+//       observer.next(3);
+
+//       let number = 1;
+
+//       const timer = setInterval(() => {
+//         observer.next(number++);
+//         if (number > 3) {
+//           clearInterval(timer);
+
+//           observer.complete(); // 表示数据流的完结
+
+//         }
+//       }, 1000);
+//     };
+//     const source$ = new Observable(onSubscribe);
+
+//     const theObserver = {
+//       next: val => console.log(val),
+//       complete: () => console.log('No More Data'),
+//     };
+//     source$.subscribe(theObserver);
+
+//   }, []);
+
+// /* ============================ Observable 的错误处理 ============================ */
+//   useEffect(() => {
+
+//     const onSubscribe = observer => {
+//       observer.next(1);
+//       observer.next(2);
+//       observer.next(3);
+
+//       try {
+//         observer.next(4);
+//       } catch (err) {
+//         observer.error(err);
+//       }
+//     };
+//     const source$ = new Observable(onSubscribe);
+
+//     const theObserver = {
+//       next: val => console.log(val),
+//       error: err => console.log(`Error: ${err}`),
+//     };
+//     source$.subscribe(theObserver);
+
+//   }, []);
+
+// /* =========================== Observable 只有一种终结状态 ========================== */
+  useEffect(() => {
+
+    const onSubscribe = observer => {
+      observer.next(1);
+      observer.next(2);
+      observer.next(3);
+
+      try {
+        observer.next(4);
+      } catch (err) {
+        observer.error(err);
+      }
+
+      observer.complete();
+
+    };
+    const source$ = new Observable(onSubscribe);
+
+    const theObserver = {
+      next: val => console.log(val),
+      error: err => console.log(`Error: ${err}`),
+      complete: () => console.log('No More Data'),
+    };
+    source$.subscribe(theObserver);
+
+  }, []);
+
+// /* ============================== Observer 的简单形式 ============================= */
+//   useEffect(() => {
+
+//     const onSubscribe = observer => {
+//       observer.next(1);
+//       observer.next(2);
+//       observer.next(3);
+
+//       try {
+//         observer.next(4);
+//       } catch (err) {
+//         observer.error(err);
+//       }
+
+//       observer.complete();
+
+//     };
+//     const source$ = new Observable(onSubscribe);
+
+//     source$.subscribe(
+//       val => console.log(val),
+//       err => console.log(err),
+//       () => console.log('No More Data')
+//     );
+
+//   }, []);
+
+// /* ============================= Observable 的退订 ============================= */
+//   useEffect(() => {
+
+//     const onSubscribe = observer => {
+//       observer.next(1);
+//       observer.next(2);
+//       observer.next(3);
+
+//       try {
+//         observer.next(4);
+//       } catch (err) {
+//         observer.error(err);
+//       }
+
+//       observer.complete();
+
+//     };
+//     const source$ = new Observable(onSubscribe);
+
+//     const subscription = source$.subscribe(
+//       val => console.log(val),
+//       err => console.log(err),
+//       () => console.log('No More Data')
+//     );
+
+//     setTimeout(() => {
+//       subscription.unsubscribe();
+//     }, 10 * 1000);
+
+//   }, []);
+
+// /* ================================ Operators =============================== */
+//   useEffect(() => {
+
+//     const onSubscribe = observer => {
+//       observer.next(1);
+//       observer.next(2);
+//       observer.next(3);
+//     };
+//     const source$ = new Observable(onSubscribe);
+
+//     source$
+//       .pipe(
+//         map(v => v * v),
+//       )
+//       .subscribe(val => console.log(val));
+
+//   }, []);
+
+
+// /* ================================= Subject ================================ */
+//   useEffect(() => {
+
+//     const tick$ = interval(1000).pipe(take(3));
+
+//     const subject = new Subject();
+
+//     tick$.subscribe(subject);
+
+//     subject.subscribe(value => console.log('observer 1: ' + value));
+
+//     setTimeout(() => {
+//       subject.subscribe(value => console.log('observer 2: ' + value));
+//     }, 1500);
+
+//   }, []);
+
+/* ================================ Scheduler =============================== */
+
   // useEffect(() => {
 
-  //   const foo = new Observable(subscriber => {
+  //   const timeStart = new Date();
+  //   const source$ = range(1, 100000);
 
-  //     subscriber.next(1);
-  //     subscriber.next(2);
-  //     subscriber.next(3);
+  //   console.log('before subscribe');
 
-  //     setTimeout(() => {
-  //       subscriber.next(4);
-  //       subscriber.complete();
-  //       subscriber.next(5);
-  //     }, 1000);
-
-  //     try {
-  //       subscriber.next(6);
-  //     } catch (err) {
-  //       subscriber.error(err);
+  //   source$.subscribe({
+  //     complete: () => {
+  //       console.log('Time elapsed: ' + (Date.now() - timeStart) + 'ms');
   //     }
-
   //   });
 
-  //   console.log('before');
-  //   foo.subscribe({
-  //     next(x) {
-  //       console.log('got value ' + x);
-  //     },
-  //     error(err) {
-  //       console.error('err: ' + err);
-  //     },
-  //     complete() {
-  //       console.log('done');
-  //     }
-  //   });
-
-  //   console.log('after');
-
-  //   console.log('----');
-  //   foo.subscribe(x => {
-  //     console.log('--', x);
-  //   });
-  //   console.log('----');
+  //   console.log('after subscribe');
 
   // }, []);
 
+  useEffect(() => {
 
-  // useEffect(() => {
+    const timeStart = new Date();
+    const source$ = range(1, 100000, asapScheduler);
 
-    // const observable = new Observable(function subscribe(subscriber) {
-    //   const intervalId = setInterval(() => {
-    //     subscriber.next('hi');
-    //   }, 1000);
+    console.log('before subscribe');
 
-    //   return function unsubscribe() {
-    //     clearInterval(intervalId);
-    //   };
-    // });
+    source$.subscribe({
+      complete: () => {
+        console.log('Time elapsed: ' + (Date.now() - timeStart) + 'ms');
+      }
+    });
 
-    // observable.subscribe(x => console.log(x));
+    console.log('after subscribe');
 
-    // observable.subscribe(
-    //   x => console.log('Observer got a next value: ' + x),
-    //   err => console.error('Observer got an error: ', + err),
-    //   () => console.log('Observer got a complete notification')
-    // );
-
-    // setTimeout(() => {
-    //   // observable.unsubscribe();
-    // }, 3 * 1000);
-
-    // map(x => x + x)(of(1, 2, 3)).subscribe(v => console.log(`value: ${v}`));
-    // 打印：
-    // value: 2
-    // value: 4
-    // value: 6
-
-    // first()(of(1, 2, 3)).subscribe(v => console.log(`value: ${v}`));
-
-    // const observable = interval(1000);
-    // const subscription = observable.subscribe(x => console.log(x));
-
-
-    // setTimeout(() => {
-    //   subscription.unsubscribe();
-    // }, 5 * 1000);
-
-  // }, []);
-
-/* ================================= Subject ================================ */
-
-  // useEffect(() => {
-  //   const subject = new Subject();
-
-  //   subject.subscribe({
-  //     next: v => console.log(`observerA: ${v}`)
-  //   });
-
-  //   subject.subscribe({
-  //     next: v => console.log(`observerB: ${v * v}`)
-  //   });
-
-  //   // subject.next(1);
-  //   // subject.next(2);
-
-  //   const observable = from([1, 2, 3]);
-
-  //   observable.subscribe(subject);
-
-/* ============================== combineLatest ============================= */
-
-  // const firstTimer = timer(0, 1000);
-
-  // const secondTimer = timer(1000, 1000);
-
-  // const firstTimerO = firstTimer.subscribe(x => console.log(`firstTimer: ${x}`));
-  // const secondTimerO = secondTimer.subscribe(x => console.log(`secondTimer: ${x}`));
-
-  // const combineTimers = combineLatest([firstTimer, secondTimer]);
-
-  // const combine = combineTimers.subscribe(val => console.log(val));
-
-  // setTimeout(() => {
-  //   firstTimerO.unsubscribe();
-  //   secondTimerO.unsubscribe();
-  //   combine.unsubscribe();
-  // }, 5 * 1000);
-
-/* ================================ switchMap =============================== */
-
-  // const source = timer(0, 5000);
-
-  // const example = source.pipe(switchMap(() => interval(500)));
-
-  // const subscribe = example.subscribe(val => console.log(val));
-
-  // setTimeout(() => {
-  //   subscribe.unsubscribe();
-  // }, 10 * 1000);
-
-  // }, []);
+  }, []);
 
   return (
-    // <RxCounter/>
-    <Container>
-      dsfsdf
-    </Container>
+    <div></div>
   );
 }
 
